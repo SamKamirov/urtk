14f597504b43f8b2fc44f1d1bab7594351e87435
CC <111481568+SamKamirov@users.noreply.github.com>
2022-11-26 22:08:57 +0500

    summary added

---
+++ js/show-menu.js
@@ -0,0 +1,21 @@
+// var mainMenuItem = document.querySelector('.menu__item--main')
+// var dropdown = document.querySelector('.dropdown-menu')
+// var dropdownItems = document.querySelectorAll('.dropdown-menu__item')
+// var dropdownItemsHidden = document.querySelectorAll('.dropdown-menu__hidden')
+
+// mainMenuItem.addEventListener('click', (evt)=> {
+//     if (evt.target.className == "menu__item menu__item--main") {
+//        dropdown.classList.toggle('show')
+//     }
+// })
+
+// for (let index = 0; index < dropdownItemsHidden.length; index++) {
+//     dropdownItems[index].addEventListener('click', ()=> {
+//         dropdownItemsHidden[index].classList.toggle('show')
+//     })
+// }
+
+// dropdown.addEventListener('mouseleave', ()=> {
+//     dropdown.classList.remove('show')
+// })
+

9ebc03e7f3aa74a81d279bb537fd4a1ee96650bc
CC <111481568+SamKamirov@users.noreply.github.com>
2022-11-29 01:30:03 +0500

    Header update
    1. Added a new dropdown menu
    2. Rebuilt paddings

--- js/script.js
+++ js/script.js
@@ -3,21 +3,11 @@
 const toggleMenu = () => {
     // const toggle = document.querySelector('.toggle')
   const toggle = document.querySelector('.toggle__menu')
-  const menu = document.querySelector('.menu')
+  const menu = document.querySelector('.menu__list')
   const textHover = document.querySelector('.menu--show')
 
   toggle.addEventListener('click', () => {
-    menu.classList.toggle('menu--active')
-  })
-
-  textHover.addEventListener('mouseenter', ()=> {
-    menu.classList.add('menu--active')
-    console.log(menu)
-  })
-
-  textHover.addEventListener('mouseout', ()=> {
-    menu.classList.remove('menu--active')
-    console.log(menu)
+    menu.classList.toggle('active')
   })
 }
 toggleMenu()

74a9e3b22c1b416292bc8950454e817018de5fa7
CC <111481568+SamKamirov@users.noreply.github.com>
2022-11-29 00:42:54 +0500

    header update
    1. Переделаны шрифты
    2. Сделана часть выпадающего меню для больших и средних экранов

--- js/script.js
+++ js/script.js
@@ -2,14 +2,23 @@
 
 const toggleMenu = () => {
     // const toggle = document.querySelector('.toggle')
-    const toggle = document.querySelector('.toggle__menu')
-    const menu = document.querySelector('.menu')
-    const infoMenu = document.querySelector('.menu--show')
+  const toggle = document.querySelector('.toggle__menu')
+  const menu = document.querySelector('.menu')
+  const textHover = document.querySelector('.menu--show')
 
-    toggle.addEventListener('click', () => {
-      menu.classList.toggle('menu--active')
-    })
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('menu--active')
+  })
+
+  textHover.addEventListener('mouseenter', ()=> {
+    menu.classList.add('menu--active')
+    console.log(menu)
+  })
+
+  textHover.addEventListener('mouseout', ()=> {
+    menu.classList.remove('menu--active')
+    console.log(menu)
+  })
 }
-
 toggleMenu()
 

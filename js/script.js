878400b444715fba25586be97a54133613eb5a33
CC <111481568+SamKamirov@users.noreply.github.com>
2022-11-29 10:14:37 +0500

    Add dropdown menu events
    Hover effects was added to dropdown menu on mobile and desktop versions

--- js/script.js
+++ js/script.js
@@ -3,12 +3,42 @@
 const toggleMenu = () => {
     // const toggle = document.querySelector('.toggle')
   const toggle = document.querySelector('.toggle__menu')
-  const menu = document.querySelector('.menu__list')
-  const textHover = document.querySelector('.menu--show')
+  const menuList = document.querySelector('.menu__list')
+  const showMenu = document.querySelector('.menu--show')
+  const menuItems = document.querySelectorAll('.list__item')
 
   toggle.addEventListener('click', () => {
-    menu.classList.toggle('active')
+    menuList.classList.toggle('active')
   })
+
+  showMenu.addEventListener('click', () => {
+    menuList.classList.toggle('active')
+  })
+
+  showMenu.addEventListener('mouseover', () => {
+    menuList.classList.add('active')
+  })
+
+  showMenu.addEventListener('mouseleave', () => {
+    menuList.classList.remove('active')
+  })
+
+  menuList.addEventListener('mouseover', () => {
+    menuList.classList.add('active')
+  })
+  
+  menuList.addEventListener('mouseleave', () => {
+    menuList.classList.remove('active')
+  })
+
+  menuItems.forEach(item => {
+    item.addEventListener('mouseover', () => {
+      item.style.backgroundColor = 'rgb(0,0,0)'
+    })
+    item.addEventListener('mouseleave', () => {
+      item.style.backgroundColor = 'rgb(255,255,255)'
+    })
+  });
 }
 toggleMenu()
 

a7ef734c49598f1e88fe0f44c7d231a896b459f9
CC <111481568+SamKamirov@users.noreply.github.com>
2022-11-29 11:05:36 +0500

    header update
    drowpown was rebuilt

--- js/script.js
+++ js/script.js
@@ -10,35 +10,6 @@ const toggleMenu = () => {
   toggle.addEventListener('click', () => {
     menuList.classList.toggle('active')
   })
-
-  showMenu.addEventListener('click', () => {
-    menuList.classList.toggle('active')
-  })
-
-  showMenu.addEventListener('mouseover', () => {
-    menuList.classList.add('active')
-  })
-
-  showMenu.addEventListener('mouseleave', () => {
-    menuList.classList.remove('active')
-  })
-
-  menuList.addEventListener('mouseover', () => {
-    menuList.classList.add('active')
-  })
-  
-  menuList.addEventListener('mouseleave', () => {
-    menuList.classList.remove('active')
-  })
-
-  menuItems.forEach(item => {
-    item.addEventListener('mouseover', () => {
-      item.style.backgroundColor = 'rgb(0,0,0)'
-    })
-    item.addEventListener('mouseleave', () => {
-      item.style.backgroundColor = 'rgb(255,255,255)'
-    })
-  });
 }
 toggleMenu()
 

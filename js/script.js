7fb67f5232a0771bdcc603c5a46beb26b2bce530
CC <111481568+SamKamirov@users.noreply.github.com>
2022-11-29 15:51:23 +0500

    Header update
    1. Added class to h1
    2. Added js events to menu
    3. Added some basic interaction to menu elements

--- js/script.js
+++ js/script.js
@@ -1,15 +1,49 @@
 //Menu
 
 const toggleMenu = () => {
-    // const toggle = document.querySelector('.toggle')
+  // const toggle = document.querySelector('.toggle')
   const toggle = document.querySelector('.toggle__menu')
   const menuList = document.querySelector('.menu__list')
   const showMenu = document.querySelector('.menu--show')
-  const menuItems = document.querySelectorAll('.list__item')
-
+  
   toggle.addEventListener('click', () => {
     menuList.classList.toggle('active')
   })
+
+  showMenu.addEventListener('mousedown', () => {
+    menuList.classList.toggle('active')
+  })
+  
+  showMenu.addEventListener('pointerover', ()=> {
+    menuList.classList.add('active')
+  })
+
+  showMenu.addEventListener('pointerleave', ()=> {
+    menuList.classList.remove('active')
+  })
+
+  menuList.addEventListener('pointerover', ()=> {
+    menuList.classList.add('active')
+  })
+
+  menuList.addEventListener('pointerleave', ()=> {
+    menuList.classList.remove('active')
+  })
 }
+
 toggleMenu()
+
+const listItems = document.querySelectorAll('.list__item')
+const menuItems = document.querySelectorAll('.menu__item')
+
+menuItems.forEach(item => {
+  item.addEventListener('pointerover', ()=> {
+    item.classList.add('current')
+    // console.log('skjghdfkjg')
+  })
+  item.addEventListener('pointerleave', ()=> {
+    item.classList.remove('current')
+  })
+});
+
 
